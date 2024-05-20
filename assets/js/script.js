653488ba6d35fc702d0147f47c5eb129151f0228
Ren Pena <rennielcpena@gmail.com>
2024-05-20 13:05:59 +0100

    Add event listeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,21 @@
 // game initialization//
-function startGame() {}
+
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.getElementsByTagName("button");
+
+  for (let button of buttons) {
+    button.addEventListener("click", function () {
+      if (this.getAttribute("data-type") === "submit") {
+        alert("You clicked Submit!");
+      } else {
+        let gameType = this.getAttribute("data-type");
+        alert(`You clicked ${gameType}`);
+      }
+    });
+  }
+});
+
+function runGame() {}
 
 function checkAnswer() {}
 
