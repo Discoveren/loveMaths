690671d228f110452bee3f41c54dd8a71ea43253
Ren Pena <rennielcpena@gmail.com>
2024-05-20 12:47:05 +0100

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// game initialization//
+function startGame() {}
+
+function checkAnswer() {}
+
+function calculateCorrectAnswer() {}
+
+function incrementScore() {}
+
+function incrementWrongAnswer() {}
+
+function displayAdditionQuestion() {}
+
+function displaySubtractQuestion() {}
+
+function displayMultiplyQuestion() {}
+
+function displayDivideQuestion() {}
